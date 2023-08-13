12f2ece73e3dafedd0f8dfb439e406d0602e78df
litonofficial61 <litonofficial61gmail.com>
2023-08-13 11:53:50 +0600

    bg color change

---
+++ main.js
@@ -0,0 +1,7 @@
+function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+}
+
+const makeBlue = document.getElementById('make-blue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'blue';
+})
